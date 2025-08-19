c0c42c95685811c25dc05cfe39ab81a420a97404
JungJungIn <call518@gmail.com>
2025-08-19 15:16:07 +0900

    Add Dockerfiles and build scripts.

---
+++ build-mcpo-server-docker-image.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+Dockerfile_PATH="./Dockerfile.MCPO-Proxy"
+IMAGE_NAME="call518/mcpo-proxy-postgresql-ops"
+
+echo "=== Building Docker image Name: ${IMAGE_NAME} ==="
+
+# CUSTOM_TAG="${1:-latest}"
+TAGs="
+1.0.0
+latest
+"
+
+for TAG in ${TAGs}
+do
+    docker build -t ${IMAGE_NAME}:${TAG} -f ${Dockerfile_PATH} .
+done
+
+echo
+
+read -p "Do you want to push the images to Docker Hub? (y/N): " answer
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    for TAG in ${TAGs}
+    do
+        docker push ${IMAGE_NAME}:${TAG}
+    done
+fi
---
+++ scripts/mcp-server-docker-cmd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+# .env 파일 로드
+ENV="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/.env"
+[ -f "$ENV" ] && set -a && . "$ENV" && set +a
+
+python -m mcp_postgresql_ops.mcp_main.py --type ${FASTMCP_TYPE} --host ${FASTMCP_HOST} --port ${FASTMCP_PORT}
